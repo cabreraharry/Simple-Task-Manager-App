85039bd59f9a01a810b37fa92d9e41aaac03347a
cabreraharry <harryericsoncabrera15@gmail.com>
2024-03-11 09:47:05 +0800

    set alerts feature added and fix

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
 const inputBox = document.getElementById("input-box");
+const datetimeInput = document.getElementById("datetime-input");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("Please write some task!");
-    }
-    else{
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Please enter a task!");
+    } else {
         let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        li.textContent = inputBox.value;
+        
+        if (datetimeInput.value !== '') {
+            li.textContent += " (Reminder: " + new Date(datetimeInput.value).toLocaleString() + ")";
+        }
+        
         listContainer.appendChild(li);
 
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
+    
     inputBox.value = '';
-    saveData()
+    datetimeInput.value = ''; // Clear datetime input after adding task
+    saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-        saveData()
-    }
-    else if(e.target.tagName == "SPAN"){
+        saveData();
+    } else if (e.target.tagName == "SPAN") {
         e.target.parentElement.remove();
-        saveData()
+        saveData();
     }
 }, false);
 
-
-function saveData(){
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
+function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
-showTask()
+showTask();
