0dc5860327b7d4218e36713c6af664a68cbbd730
cabreraharry <harryericsoncabrera15@gmail.com>
2024-03-11 09:30:34 +0800

    Revert "Set Alerts Feature added"
    This reverts commit 5dde38df3d8da889a327914b8dad8a3b6c480dd9.

--- script.js
+++ script.js
@@ -8,34 +8,18 @@ function addTask(){
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-
-        let setAlertsButton = document.createElement("button");
-        setAlertsButton.textContent = "Set Alerts";
-        setAlertsButton.classList.add("set-alerts-button");
-        setAlertsButton.addEventListener("click", function() {
-            // Call a function to handle setting alerts for this task
-            // You can define this function according to your application's logic
-            // For example: setAlertsForTask(li);
-        });
-        li.appendChild(setAlertsButton);
-
-        let crossIcon = document.createElement("span");
-        crossIcon.innerHTML = "\u00d7";
-        crossIcon.addEventListener("click", function() {
-            li.remove();
-            saveData();
-        });
-        li.appendChild(crossIcon);
-
         listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = '';
-    saveData();
+    saveData()
 }
 
-
-listContainer.addEventListener("click", function(e) {
-    if (e.target.tagName === "LI") {
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
         saveData()
     }
